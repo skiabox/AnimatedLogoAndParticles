224ebeb4fb79914e73819743d19d947860006f84
Stavros Kefaleas <stavros.kefaleas@gmail.com>
2020-01-26 16:44:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// const logo = document.querySelectorAll('#logo path');
-
-// console.log(logo);
-
-// for(let i = 0; i < logo.length; i++) {
-//   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-// }
 const particles = [];
 
 function setup() {
